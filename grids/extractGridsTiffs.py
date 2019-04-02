a16a4fc8e67c521525c20263d6bf49e0669dd5d1
ayushgupt <ayushabg@gmail.com>
2019-04-02 22:11:52 +0000

    goa rough pipeline built

---
+++ grids/extractGridsTiffs.py
@@ -0,0 +1,52 @@
+import json
+import sys
+import rasterio
+from rasterio.mask import mask
+from json import loads
+import sys
+import os
+from os import listdir
+from os.path import isfile, join
+# from rasterio.tools.mask import mask
+
+# tiffFileName=sys.argv[1]
+# folder_containing_tifffiles = "data/copyDummyData"
+folder_containing_tifffiles = sys.argv[1]
+jsonFileName = sys.argv[2]
+output_directory = folder_containing_tifffiles+'_cutFiles'
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+allTiffFiles = [f for f in listdir(folder_containing_tifffiles) if isfile(join(folder_containing_tifffiles, f))]
+
+jsonFileList=[jsonFileName]
+
+
+for tiffFileName in allTiffFiles:
+    for jsonFileName in jsonFileList:
+        stateData = json.loads(open(jsonFileName).read())
+        print('tiffFileName',tiffFileName)
+        print('jsonFileName',jsonFileName)
+        for currVillageFeature in stateData["features"]:
+            try:
+                #vCode2011=currVillageFeature["properties"]["village_code_2011"]
+                #vCode2001=currVillageFeature["properties"]["village_code_2001"]
+                vId=currVillageFeature["properties"]["ID"]
+                geoms=currVillageFeature["geometry"]
+                listGeom=[]
+                listGeom.append(geoms)
+                geoms=listGeom
+                with rasterio.open(folder_containing_tifffiles+'/'+tiffFileName) as src:
+                    out_image, out_transform = mask(src, geoms, crop=True)
+
+                out_meta = src.meta.copy()
+                # save the resulting raster  
+                out_meta.update({"driver": "GTiff",
+                    "height": out_image.shape[1],
+                    "width": out_image.shape[2],
+                "transform": out_transform})
+                saveFileName=output_directory+'/'+tiffFileName[:-4]+"@"+vId+".tif"
+                #print(vCode2011)
+                with rasterio.open(saveFileName, "w", **out_meta) as dest:
+                    dest.write(out_image)
+            except:
+                continue
---
+++ grids/makeRgbLandsat.py
@@ -0,0 +1,30 @@
+from libtiff import TIFF
+import sys
+import numpy as np
+import os
+from PIL import Image
+from os import listdir
+from os.path import isfile, join
+import pickle
+import h5py
+import scipy.misc
+
+
+inputFolder=sys.argv[1]
+outputFolder=sys.argv[2]
+os.makedirs(outputFolder, exist_ok=True)
+onlyfiles = [f for f in listdir(inputFolder) if isfile(join(inputFolder, f))]
+
+
+for currImageName in onlyfiles:
+	# currImageName='kerela-0000000000-0000000000@10400.0@KL-113@627160.tif'
+	destImageName=currImageName
+	tif = TIFF.open(inputFolder+'/'+currImageName, mode='r')
+	image = tif.read_image()
+	data = np.array(image)
+	scipy.misc.imsave(outputFolder+'/'+destImageName[:-4]+'.png', data[:,:,0:3])
+
+
+
+
+
